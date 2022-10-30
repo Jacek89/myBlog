c866d1a26cbf8b5304c396e13f788bbd302f6aca
Jacek Skoczek <jaca1989@gmail.com>
2022-10-30 12:19:46 +0100

    SQLite changed to Postgresql

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myblog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL").replace("postgres://", "postgresql://")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
